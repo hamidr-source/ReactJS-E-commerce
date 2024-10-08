3f712489b8e4c1ea81d36558342ac1be0e62840f
hamidr-source <hr454837@gmail.com>
2024-10-08 13:28:18 +0330

    Add some features in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
 import "./Footer.css"
 
 const Footer = () => {
@@ -11,6 +13,10 @@ const Footer = () => {
         <InstagramIcon sx={{ fontSize: 35 }} />
         <TelegramIcon sx={{ fontSize: 35 }} />
         <YouTubeIcon sx={{ fontSize: 35 }} />
+      </div>
+      <div className='left-footer'>
+        <p><PhoneIcon sx={{ fontSize: 35 }} /> ******</p>
+        <p><EmailIcon sx={{ fontSize: 35 }} /> example@gmail.com</p>
       </div>
     </footer>
   )
