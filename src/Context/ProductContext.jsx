b84b426fe6ac633686d22876bdc549c5077f174c
hamidr-source <hr454837@gmail.com>
2024-10-10 12:24:00 +0330

    Add user login

--- src/Context/ProductContext.jsx
+++ src/Context/ProductContext.jsx
@@ -6,6 +6,7 @@ export const useProducts = () => useContext(ProductContext);
 
 const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [users, setUsers] = useState("");
 
   useEffect(() => {
     axios
@@ -14,10 +15,17 @@ const ProductProvider = ({ children }) => {
         (axios.defaults.headers.common["Authorization"] = 555)
       )
       .then((res) => setProducts(res.data));
+
+    axios
+      .get(
+        "https://fakestoreapi.com/users",
+        (axios.defaults.headers.common["Authorization"] = 555)
+      )
+      .then((response) => setUsers(response.data));
   }, []);
 
   return (
-    <ProductContext.Provider value={{ products }}>
+    <ProductContext.Provider value={{ products, users }}>
       {children}
     </ProductContext.Provider>
   );
--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,27 +1,52 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./LoginPage.css"
+import "./LoginPage.css";
+import { useProducts } from "../../Context/ProductContext";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 const LoginPage = () => {
-  const [username, setUsername] = useState("");
-
+  const [userEmail, setUserEmail] = useState("");
+  const { users } = useProducts();
   const navigate = useNavigate();
 
   function handleSignIn(e) {
     e.preventDefault();
+    const currentUser = users.find((user) => {
+      return user.email === userEmail;
+    });
+    console.log(users);
+    users.forEach((user) => {
+      if (!currentUser) {
+        return (
+          <Stack sx={{ width: "100%" }} spacing={2}>
+            <Alert severity="error">This is an error Alert.</Alert>
+          </Stack>
+        );
+      } else if (
+        currentUser.password === user.password &&
+        currentUser.email === user.email
+      ) {
+        navigate("/");
+      }
+    });
   }
 
   return (
     <div className="login-page">
       <div className="form">
         <form className="login-form">
-          <input type="email" className="email-input" placeholder="email" />
+          <input
+            type="email"
+            className="email-input"
+            placeholder="email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+          />
           <input
             type="text"
             className="username-input"
             placeholder="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
           />
           <input
             type="password"
