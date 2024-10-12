337d305ead69b71768c289361c209dfafdc3934b
hamidr-source <hr454837@gmail.com>
2024-10-12 10:53:05 +0330

    Add log out button in dashboard

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { Cookies } from 'react-cookie'
+import React from "react";
+import { Cookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import LogoutIcon from '@mui/icons-material/Logout';
+import "./Dashboard.css"
 
 const Dashboard = () => {
-  const cookies = new Cookies()
-
+  const cookies = new Cookies();
+  const navigate = useNavigate();
+  const handleRemove = () => {
+    cookies.remove('user', { path: '/', domain: 'localhost' });
+    navigate("/login")
+  };
+  console.log(cookies.cookies)
   return (
-    <h1>Hey Dear {cookies.cookies.user}</h1>
-  )
-}
+    <div className="dashboard">
+      <h1>Hey Dear {cookies.cookies.user}</h1>{" "}
+      <Button variant="outlined" color="error" onClick={handleRemove}>
+        Log Out <LogoutIcon />
+      </Button>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
