552876df9e99d46701ef62935888ca3e0b99541e
hamidr-source <hr454837@gmail.com>
2024-10-15 13:34:35 +0330

    Refactor the theme changer code

--- src/Components/ThemeChanger/ThemeChanger.jsx
+++ src/Components/ThemeChanger/ThemeChanger.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
 import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
 
 const ThemeChanger = () => {
-  let theme = localStorage.getItem("theme");
+  let [theme, setTheme] = useState(localStorage.getItem("theme"));
 
-  window.addEventListener("load", () => {
+  useEffect(() => {
     let localStorageOnloadData = localStorage.getItem("theme");
     if (localStorageOnloadData === "dark") setDarkMode();
     else setLightMode();
-  });
+  }, [theme]);
 
   const handleThemechange = () => {
     let localStorageData = localStorage.getItem("theme");
@@ -21,29 +21,25 @@ const ThemeChanger = () => {
   };
 
   const setLightMode = () => {
-    theme = "light";
+    setTheme("light");
     document.body.style.color = "#000";
     document.body.style.backgroundColor = "#fff";
     localStorage.setItem("theme", "light");
   };
 
   const setDarkMode = () => {
-    theme = "dark";
+    setTheme("dark");
     document.body.style.color = "#fff";
     document.body.style.backgroundColor = "#2e2e47b0";
     localStorage.setItem("theme", "dark");
   };
 
   return (
-    <div
-      className="them-changer"
-      onClick={handleThemechange}
-      defaultChecked="light"
-    >
+    <div className="theme-changer" onClick={handleThemechange}>
       {theme === "light" ? (
-        <LightModeRoundedIcon fontSize="large" />
-      ) : (
         <DarkModeRoundedIcon fontSize="large" />
+      ) : (
+        <LightModeRoundedIcon fontSize="large" />
       )}
     </div>
   );
