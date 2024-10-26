ba06b1ab254df2b73abb5566f5fe27c3cdf07572
hamidr-source <hr454837@gmail.com>
2024-10-26 15:11:27 +0330

    Add toggle for open category section

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import "./Category.css"
+import "./Category.css";
+import { useProducts } from "../../Context/ProductContext";
 
 const Category = () => {
+  const { products } = useProducts();
+  const [isOpen, setIsOpen] = useState(false)
+  function handleOpenCategorySection() {
+    setIsOpen(!isOpen)
+    products.map((product) => {
+        console.log(product.category)
+    })
+  }
   return (
-    <div className="category">
+    <div className="category" onClick={handleOpenCategorySection}>
       Category <KeyboardArrowDownIcon />
+      <div className={isOpen ? "category-box open" : "category-box close"}>
+        <span>men's clothing</span>
+        <span>jewelery</span>
+        <span>electronics</span>
+        <span>women's clothing</span>
+      </div>
     </div>
   );
 };
