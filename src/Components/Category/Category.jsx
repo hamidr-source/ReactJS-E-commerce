fbbad430218c2b18b5918cb42d2a54d8c5cacfb1
hamidr-source <hr454837@gmail.com>
2024-10-28 13:17:22 +0330

    Add product categories in category box

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,25 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import "./Category.css";
 import { useProducts } from "../../Context/ProductContext";
 
 const Category = () => {
   const { products } = useProducts();
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+     const categoriesSet = new Set()
+
+     products.forEach(product => {
+      categoriesSet.add(product.category)
+     });
+
+     setCategories([...categoriesSet])
+    }, [products])
+
   function handleOpenCategorySection() {
-    setIsOpen(!isOpen)
-    products.map((product) => {
-        console.log(product.category)
-    })
+    setIsOpen(!isOpen);
   }
+
   return (
     <div className="category" onClick={handleOpenCategorySection}>
       Category <KeyboardArrowDownIcon />
       <div className={isOpen ? "category-box open" : "category-box close"}>
-        <span>men's clothing</span>
-        <span>jewelery</span>
-        <span>electronics</span>
-        <span>women's clothing</span>
+        {categories.map((category, index) => (
+          <p className="categories" key={index}>{category}</p>
+        ))}
       </div>
     </div>
   );
