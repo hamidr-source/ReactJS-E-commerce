260bdc6ccf8b261256654a1f86a4718ba626cf0e
hamidr-source <hr454837@gmail.com>
2024-10-31 21:25:28 +0330

    Add category link for category page

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import "./Category.css";
 import { useProducts } from "../../Context/ProductContext";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Category = () => {
   const { products } = useProducts();
   const [isOpen, setIsOpen] = useState(false);
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-     const categoriesSet = new Set()
+  useEffect(() => {
+    const categoriesSet = new Set();
 
-     products.forEach(product => {
-      categoriesSet.add(product.category)
-     });
+    products.forEach((product) => {
+      categoriesSet.add(product.category);
+    });
 
-     setCategories([...categoriesSet])
-    }, [products])
+    setCategories([...categoriesSet]);
+  }, [products]);
 
   function handleOpenCategorySection() {
     setIsOpen(!isOpen);
@@ -24,10 +27,21 @@ const Category = () => {
 
   return (
     <div className="category" onClick={handleOpenCategorySection}>
-      Category <KeyboardArrowDownIcon />
+      <div className="select">
+        <span>Category</span>
+        <span>
+          {isOpen ? (
+            <KeyboardArrowUpIcon sx={{ fontSize: 24 }} />
+          ) : (
+            <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
+          )}
+        </span>
+      </div>
       <div className={isOpen ? "category-box open" : "category-box close"}>
         {categories.map((category, index) => (
-          <p className="categories" key={index}>{category}</p>
+          <Link key={index} to={`/category/${category.split(` `).join(`-`).toLowerCase()}`}>
+            <p className="categories">{category}</p>
+          </Link>
         ))}
       </div>
     </div>
