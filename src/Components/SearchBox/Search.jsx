7053cea37f61c4b67bf2fba2d5a8ef08fd627bfa
hamidr-source <hr454837@gmail.com>
2024-11-02 09:52:24 +0330

    Fix result search box bug

--- src/Components/SearchBox/Search.jsx
+++ src/Components/SearchBox/Search.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useProducts } from "../../Context/ProductContext";
 
 const Search = () => {
   const { products } = useProducts();
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [query, setQuery] = useState("");
+  const [focus, setFocus] = useState(false);
+  const navigate = useNavigate();
 
   function handleSearchProduct(event) {
     const serachQuery = event.target.value;
@@ -19,23 +21,52 @@ const Search = () => {
     setFilteredProducts(filtered);
   }
 
+  function handleFocus() {
+    setFocus(true);
+  }
+
+  function handleProductClick(productId, event) {
+    event.stopPropagation()
+    console.log(event.isPropagationStopped());
+    navigate(`/product/${productId}`);
+  }
+
+  function handleBlur() {
+    setFocus(false);
+  }
+
   return (
     <div className="search-box">
       <input
         placeholder="Search Here ..."
         value={query}
         onChange={handleSearchProduct}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
       <Link>
         <SearchIcon />
       </Link>
       <div className="complete-box">
-        {filteredProducts.length > 0 && query ? (
-          filteredProducts.map((product, index) => (
-            <li key={index}>{product.title}</li>
-          ))
-        ) : (
-          <li>Product Not Found</li>
+        {focus && (
+          <div>
+            {query === "" ? (
+              <p>Just search everything you want</p>
+            ) : filteredProducts.length > 0 ? (
+              <ul>
+                {filteredProducts.map((product) => (
+                  <li
+                    key={product.id}
+                    onMouseDown={(event) => handleProductClick(product.id, event)}
+                  >
+                    {product.title}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>Product Not Found</p>
+            )}
+          </div>
         )}
       </div>
     </div>
