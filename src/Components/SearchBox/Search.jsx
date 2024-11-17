fa971be0b9799d903215b12dd1be602b477991ee
hamidr-source <hr454837@gmail.com>
2024-11-17 13:12:48 +0330

    Add search box for mobile

--- src/Components/SearchBox/Search.jsx
+++ src/Components/SearchBox/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import { useProducts } from "../../Context/ProductContext";
 import ResultBox from "./ResultBox";
@@ -10,8 +10,29 @@ const Search = () => {
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [query, setQuery] = useState("");
   const [focused, setFocused] = useState(false);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [isModalOPen, setIsModalOPen] = useState(false);
   const [productLimit, setProductLimit] = useState([]);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkWindowSize = () => {
+      const width = window.innerWidth;
+      const maxWidth = 426;
+
+      if (width > maxWidth) {
+        setMobileMenu(true);
+      } else {
+        setMobileMenu(false);
+      }
+    };
+
+    checkWindowSize();
+    window.addEventListener("resize", checkWindowSize);
+    return () => {
+      window.removeEventListener("resize", checkWindowSize);
+    };
+  }, []);
 
   function handleSearchProduct(event) {
     const serachQuery = event.target.value;
@@ -33,18 +54,34 @@ const Search = () => {
     navigate("/searchResult", { state: { products: filteredProducts } });
   }
 
+  function handleCloseModal() {
+    setIsModalOPen(false);
+    setFocused(false);
+    document.body.style.overflow = 'auto';
+  }
+
+  function handleOpenModal() {
+    setIsModalOPen(true);
+    setFocused(true);
+    document.body.style.overflow = 'hidden';
+  }
+
   return (
-    <div className="search-box">
-      <div className="search-input">
-        <input
-          placeholder="Search Here ..."
-          value={query}
-          onChange={handleSearchProduct}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-        />
-        <SearchIcon />
-      </div>
+    <div className={mobileMenu ? "search-box" : "mobile-search-box"}>
+      {mobileMenu ? (
+        <div className="search-input">
+          <input
+            placeholder="Search Here ..."
+            value={query}
+            onChange={handleSearchProduct}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+          />
+          <SearchIcon />
+        </div>
+      ) : (
+        <SearchIcon onClick={handleOpenModal} />
+      )}
 
       <div className={focused ? "result-box-open" : "result-box-close"}>
         {focused && (
@@ -72,6 +109,29 @@ const Search = () => {
           </div>
         )}
       </div>
+      {isModalOPen && (
+        <div className="modal">
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <input type="text" value={query} onChange={handleSearchProduct} />
+            <button onClick={handleCloseModal}>Close</button>
+            <div className="search-results">
+              <div>
+                {query === "" ? (
+                  <p>Just search anything you want</p>
+                ) : filteredProducts.length > 0 ? (
+                  <div>
+                    {filteredProducts.map((product) => (
+                      <ResultBox {...product} key={product.id} />
+                    ))}
+                  </div>
+                ) : (
+                  <p>Product Not Found</p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
